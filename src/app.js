a01d8866f186f9ee4b0fcaf33295b81bbc56f6b7
Gustavo <gustavo303a@gmail.com>
2024-10-18 19:00:06 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const router = require('./routes/rotas');
+const express = require("express");
+const cors = require("cors"); 
+const router = require("./routes/rotas");
 
-const app = express(); 
+const app = express();
 
+app.use(cors()); // Adicionar middleware CORS
 app.use(express.json());
 
 // Usar as rotas
@@ -10,5 +12,5 @@ app.use(router);
 
 // Iniciar o servidor
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3001');
+  console.log("Servidor rodando na porta 3000");
 });
